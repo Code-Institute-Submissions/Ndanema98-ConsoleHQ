5712981d81dc677789b01e807736f82d1e73aab1
Ndanema98 <davidnda26@yahoo.co.uk>
2023-06-07 18:53:40 +0000

    Feat: Add cache control

--- console_hq/settings.py
+++ console_hq/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['consolehq.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['consolehq.herokuapp.com', 'localhost', '8000-ndanema98-consolehq-s9a9suessnw.ws-eu98.gitpod.io']
 
 
 # Application definition
@@ -179,6 +179,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'consolehq'
     AWS_S3_REGION_NAME = 'us-east-1'
