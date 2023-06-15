c33ea1cf9e05ebaa785dc40faa2afd35ef2ab898
Ndanema98 <davidnda26@yahoo.co.uk>
2023-06-15 14:06:14 +0000

    Feat: Add footer and facebook business page

--- console_hq/settings.py
+++ console_hq/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['consolehq.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['consolehq.herokuapp.com', 'localhost', '8000-ndanema98-consolehq-s9a9suessnw.ws-eu99.gitpod.io']
 
 
 # Application definition
