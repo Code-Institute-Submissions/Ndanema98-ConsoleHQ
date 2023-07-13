a60622c4e84393d8cab88ceafe2679f4a71b8169
Ndanema98 <davidnda26@yahoo.co.uk>
2023-07-13 13:15:10 +0000

    Style: Finish styling for add review form

--- console_hq/settings.py
+++ console_hq/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['consolehq.herokuapp.com', 'localhost', '8000-ndanema98-consolehq-9al2q67n02a.ws-eu101.gitpod.io',]
 
